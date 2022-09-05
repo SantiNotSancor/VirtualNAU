f1557fe1b37b8829fcb4144de04fa1f66b8106ae
MichatNovato <valentinmichat04@gmail.com>
2022-09-05 14:20:54 -0300

    (Nahuel) add stock

--- client/src/Vendor/addStock.js
+++ client/src/Vendor/addStock.js
@@ -2,10 +2,6 @@ import React, { Component, useEffect, useState } from 'react';
 import { ModalOpener } from '../modalOpener';
 import { Request, TaskRequest } from '../textInputs';
 import Axios from 'axios';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
 import moment from 'moment';
 //import image from './Images/addStock.svg';
 
@@ -41,12 +37,12 @@ export class AddStock extends Component {
 
     myForm() {
         return (
-            <Form onSubmit={e => e.preventDefault()}>
+            <form onSubmit={e => e.preventDefault()}>
                 {/* Se hará un formulario para que el usuario complete los datos de la materia prima; fecha actual, responsable, nombre, descripción y cantidad  */}
                 <Request toShow="responsible" onChange={(event) => {
                     this.setState({ responsible: event.target.value });
                 }} />
-                <Table striped bordered id="taskTable">
+                <table striped bordered id="taskTable">
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -59,18 +55,17 @@ export class AddStock extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* {!this.state.materials[0]? null : */}
-                        {this.state.materials.map((material, index) => {console.log('hi');
+                        {!this.state.materials[0]? ()=> null :
+                        this.state.materials.map((material, index) =>
 //Debería tomar la fila dada por Row y agregarsela a la lista de materias primas que posee el componente. De estar esta
 //ya ingresada (en caso de una modificación), reemplazarle. En caso de eliminarse una fila, aparecerá la cantidad como -1
                         <Row onRemove={this.removeMaterial(index)} onChange={(e, property) => {
                             this.changeMaterial(index, e, property);
                             this.addMaterial();
-                        }} /> })
-                        }
+                        }} />)}
                     </tbody>
-                </Table>
-            </Form>
+                </table>
+            </form>
         );
     }
 
@@ -86,53 +81,54 @@ export const Row = ({ onChange, onRemove }) => {
 
     const [input, setInput] = useState({ id: '', name: '', description: '', quantity: '', weight: '', meters: '' });
 
-    useEffect(() => {
-        onChange(input.id, 'id');
-        onChange(input.id, 'quantity');
-    }, [input]);
+    // useEffect(() => {
+    //     onChange(input.id, 'id');
+    //     onChange(input.id, 'quantity');
+    // }, [input]);
     //TODO:
     return (
-        <tr style={{ 'backgroundColor': 'green' }}>{/*Muestra el primero*/}
-            <td><FormControl value={input.id}//ID
-                onChange={(e) => {
-                    let aux = { ...input };
-                    aux.id = e.target.value;
-                    setInput({ ...aux });
-                }} />
-            </td>
-            <td><FormControl value={input.name}//Nombre
-                onChange={(e) => {
-                    let aux = { ...input };
-                    aux.name = e.target.value;
-                    setInput({ ...aux });
-                }} /></td>
-            <td><FormControl value={input.description}//Descripción
-                onChange={(e) => {
-                    let aux = { ...input };
-                    aux.description = e.target.value;
-                    setInput({ ...aux });
-                }} /></td>
-            <td><FormControl value={input.quantity}//Cantidad
-                onChange={(e) => {
-                    if (isNaN(e.target.value) || e.target.value < 0)
-                        return;
-                    let aux = { ...input };
-                    aux.quantity = e.target.value;
-                    setInput({ ...aux });
-                }} /></td>
-            <td><FormControl value={input.weight}//Peso
-                onChange={(e) => {
-                    let aux = { ...input };
-                    aux.weight = e.target.value;
-                    setInput({ ...aux });
-                }} /></td>
-            <td><FormControl value={input.meters}//Metros
-                onChange={(e) => {
-                    let aux = { ...input };
-                    aux.meters = e.target.value;
-                    setInput({ ...aux });
-                }} /></td>
-            <td><Button onClick={onRemove}>X</Button></td>
-        </tr>
+        <tr><td>Hello</td></tr>
+        // <tr style={{ 'backgroundColor': 'green' }}>{/*Muestra el primero*/}
+        //     <td><input type="text" value={input.id}//ID
+        //         onChange={(e) => {
+        //             let aux = { ...input };
+        //             aux.id = e.target.value;
+        //             setInput({ ...aux });
+        //         }} />
+        //     </td>
+        //     <td><input type="text" value={input.name}//Nombre
+        //         onChange={(e) => {
+        //             let aux = { ...input };
+        //             aux.name = e.target.value;
+        //             setInput({ ...aux });
+        //         }} /></td>
+        //     <td><input type="text" value={input.description}//Descripción
+        //         onChange={(e) => {
+        //             let aux = { ...input };
+        //             aux.description = e.target.value;
+        //             setInput({ ...aux });
+        //         }} /></td>
+        //     <td><input type="number" value={input.quantity}//Cantidad
+        //         onChange={(e) => {
+        //             if (isNaN(e.target.value) || e.target.value < 0)
+        //                 return;
+        //             let aux = { ...input };
+        //             aux.quantity = e.target.value;
+        //             setInput({ ...aux });
+        //         }} /></td>
+        //     <td><input type="number" value={input.weight}//Peso
+        //         onChange={(e) => {
+        //             let aux = { ...input };
+        //             aux.weight = e.target.value;
+        //             setInput({ ...aux });
+        //         }} /></td>
+        //     <td><input type="number" value={input.meters}//Metros
+        //         onChange={(e) => {
+        //             let aux = { ...input };
+        //             aux.meters = e.target.value;
+        //             setInput({ ...aux });
+        //         }} /></td>
+        //     <td><button onClick={onRemove}>X</button></td>
+        // </tr>
     );
 }
--- client/src/Vendor/index.js
+++ client/src/Vendor/index.js
@@ -4,17 +4,17 @@ import {NewQuery} from './newQuery';
 import { ViewQuery } from './viewQuery';
 // import {TickSuspended} from './tickSuspended';
 // import {TickToDos} from './tickToDos';
-import './styles.css';
+// import './styles.css';
 
 export default class Vendor extends Component {
   render() {
     return (
       <>
-        <style>{'body {background-color: #20154D; background-Image: url(http://localhost:3000/images/waveVendor.png); background-repeat: no-repeat; background-size: contain;'}</style>
+        {/* <style>{'body {background-color: #20154D; background-Image: url(http://localhost:3000/images/waveVendor.png); background-repeat: no-repeat; background-size: contain;'}</style>
         <h2 className='indexVendorh2'>¿Qué desea hacer?</h2>
-        <div><NewQuery/></div>
+        <div><NewQuery/></div> */}
         <div><AddStock/></div>
-        <div><ViewQuery/></div>
+        {/* <div><ViewQuery/></div> */}
         {/*<TickSuspended />
         <TickToDo /> */}
       </>
